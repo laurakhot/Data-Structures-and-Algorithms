a9004799eb1c656ab254b037ae35395632aa2a6e
laurakhot <137143005+laurakhot@users.noreply.github.com>
2024-03-03 17:24:09 -0800

    Kruskal Initial

--- mazes/src/disjointsets/UnionBySizeCompressingDisjointSets.java
+++ mazes/src/disjointsets/UnionBySizeCompressingDisjointSets.java
@@ -1,6 +1,8 @@
 package disjointsets;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+// import java.util.List;
 
 /**
  * A quick-union-by-size data structure with path compression.
@@ -8,7 +10,9 @@ import java.util.List;
  */
 public class UnionBySizeCompressingDisjointSets<T> implements DisjointSets<T> {
     // Do NOT rename or delete this field. We will be inspecting it directly in our private tests.
-    List<Integer> pointers;
+    ArrayList<Integer> pointers;
+    private int size;
+    private HashMap<T, Integer> ids;
 
     /*
     However, feel free to add more fields and private helper methods. You will probably need to
@@ -16,25 +20,61 @@ public class UnionBySizeCompressingDisjointSets<T> implements DisjointSets<T> {
     */
 
     public UnionBySizeCompressingDisjointSets() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        this.pointers = new ArrayList<>();
+        this.size = 0;
+        this.ids = new HashMap<>();
+
     }
 
     @Override
     public void makeSet(T item) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        pointers.add(size, -1);
+        ids.put(item, size);
+        size++;
     }
 
     @Override
     public int findSet(T item) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (!ids.containsKey(item)) {
+            throw new IllegalArgumentException();
+        }
+        int index = ids.get(item);
+        // finding the root
+        while (pointers.get(index) >= 0) {
+            index = pointers.get(index);
+        }
+        // path compression
+        int rootIndex = index; // what will return
+        index = ids.get(item);
+        while (index != rootIndex) {
+            int val = pointers.get(index);
+            pointers.set(index, rootIndex);
+            index = val;
+        }
+        return rootIndex;
     }
 
     @Override
     public boolean union(T item1, T item2) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (!ids.containsKey(item1) || !ids.containsKey(item2)) {
+            throw new IllegalArgumentException();
+        }
+        int root1 = findSet(item1);
+        int root2 = findSet(item2);
+        // in same set
+        if (root1 == root2) {
+            return false;
+        }
+        // union by size
+        if (pointers.get(root1) * -1 > pointers.get(root2) * -1) {
+            int add = pointers.get(root2);
+            pointers.set(root2, root1);
+            pointers.set(root1, pointers.get(root1) + add);
+        } else {
+            int add = pointers.get(root1);
+            pointers.set(root1, root2);
+            pointers.set(root2, pointers.get(root2) + add);
+        }
+        return true;
     }
 }
--- mazes/src/graphs/minspantrees/KruskalMinimumSpanningTreeFinder.java
+++ mazes/src/graphs/minspantrees/KruskalMinimumSpanningTreeFinder.java
@@ -1,12 +1,15 @@
 package graphs.minspantrees;
 
 import disjointsets.DisjointSets;
-import disjointsets.QuickFindDisjointSets;
+// import disjointsets.QuickFindDisjointSets;
+import disjointsets.UnionBySizeCompressingDisjointSets;
 import graphs.BaseEdge;
 import graphs.KruskalGraph;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -17,12 +20,12 @@ public class KruskalMinimumSpanningTreeFinder<G extends KruskalGraph<V, E>, V, E
     implements MinimumSpanningTreeFinder<G, V, E> {
 
     protected DisjointSets<V> createDisjointSets() {
-        return new QuickFindDisjointSets<>();
-        /*
-        Disable the line above and enable the one below after you've finished implementing
-        your `UnionBySizeCompressingDisjointSets`.
-         */
-        // return new UnionBySizeCompressingDisjointSets<>();
+        // return new QuickFindDisjointSets<>();
+        // /*
+        // Disable the line above and enable the one below after you've finished implementing
+        // your `UnionBySizeCompressingDisjointSets`.
+        //  */
+        return new UnionBySizeCompressingDisjointSets<>();
 
         /*
         Otherwise, do not change this method.
@@ -33,15 +36,34 @@ public class KruskalMinimumSpanningTreeFinder<G extends KruskalGraph<V, E>, V, E
 
     @Override
     public MinimumSpanningTree<V, E> findMinimumSpanningTree(G graph) {
-        // Here's some code to get you started; feel free to change or rearrange it if you'd like.
-
         // sort edges in the graph in ascending weight order
         List<E> edges = new ArrayList<>(graph.allEdges());
         edges.sort(Comparator.comparingDouble(E::weight));
 
-        DisjointSets<V> disjointSets = createDisjointSets();
+        if (graph.allVertices().isEmpty()) {
+            return new MinimumSpanningTree.Success<>();
+        }
 
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        DisjointSets<V> disjointSets = createDisjointSets(); // individual sets of vertices
+        for (V vertex: graph.allVertices()) {
+            disjointSets.makeSet(vertex);
+        }
+
+        HashSet<E> temp = new HashSet<>();
+
+        for (E edge : edges) {
+            V u = edge.from();
+            V v = edge.to();
+            int uMST = disjointSets.findSet(v);
+            int vMST = disjointSets.findSet(u);
+            if (uMST != vMST) {
+                temp.add(edge);
+                disjointSets.union(u, v);
+            }
+        }
+        if (temp.size() == graph.allVertices().size() - 1) {
+            return new MinimumSpanningTree.Success<>(temp);
+        }
+        return new MinimumSpanningTree.Failure<>(); //if MST not connected then MST doesn't exit
     }
 }
